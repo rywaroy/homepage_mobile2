299c08ee075caacb239f83c45c272c8b2525d38e
zhangzhihao <464485682@qq.com>
2019-02-21 15:27:31 +0800

    add: header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <div class="header__back">
+      <div class="header__back-icon" v-if="back" @click="goBack()"></div>
+    </div>
+    <div class="header__title text-ovh">{{title}}</div>
+    <div class="header__menu" @click="openMenu()"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    back: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    openMenu() {
+      this.$store.commit('showMenu', !this.isShowMenu);
+    },
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+  computed: {
+    isShowMenu() {
+      return this.$store.state.isShowMenu;
+    },
+  },
+};
+</script>
+
+<style>
+@import './header.css';
+</style>
