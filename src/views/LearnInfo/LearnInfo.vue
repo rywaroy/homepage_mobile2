60b1e268f248999962a8c23f8bca1e79030443d1
zhangzhihao <464485682@qq.com>
2019-02-25 16:30:39 +0800

    Add: learn 详情

---
+++ src/views/LearnInfo/LearnInfo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <my-header :title="info.title" :back="true"></my-header>
+    <div class="m-content scroll">
+      <div class="learn-content text-break markdown-body" v-html="info.html"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header/Header.vue';
+import { apiGetLearnInfo } from '@/api/api';
+
+export default {
+  name: 'learnInfo',
+  data() {
+    return {
+      info: {},
+    };
+  },
+  mounted() {
+    this.getInfo();
+  },
+  methods: {
+    getInfo() {
+      apiGetLearnInfo(this.$route.params.id)
+        .then(res => {
+          this.info = res.data.data;
+        });
+    },
+  },
+  components: {
+    MyHeader: Header,
+  },
+};
+</script>
+
+<style>
+@import './markdown.css';
+@import './learninfo.css';
+</style>
