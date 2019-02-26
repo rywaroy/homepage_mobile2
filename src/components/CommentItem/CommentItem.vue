9d48396161efe301886b0efcfb46ab36ffc41ac5
zhangzhihao <464485682@qq.com>
2019-02-26 15:54:43 +0800

    Add: comment component

---
+++ src/components/CommentItem/CommentItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="comment">
+    <div class="comment__top">
+      <div class="comment__user">
+        <div class="comment__avatar bg-cover" v-if="avatar" :style="{backgroundImage:`url(avatar)`}"></div>
+        <div class="comment__name">{{name}}</div>
+      </div>
+      <div class="comment__time">{{time | time}}</div>
+    </div>
+    <div class="comment__content">{{content}}</div>
+  </div>
+</template>
+<style>
+  @import './commentitem.css';
+</style>
+<script>
+export default {
+  props: {
+    avatar: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    time: {
+      type: [String, Number],
+    },
+    content: {
+      type: String,
+    },
+  },
+};
+</script>
