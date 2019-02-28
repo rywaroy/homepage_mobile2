9ca5badfb2fb0cc71db69d50b8dba774a5bb0c39
zhangzhihao <464485682@qq.com>
2019-02-28 17:42:19 +0800

    fix: bug

--- src/components/CommentItem/CommentItem.vue
+++ src/components/CommentItem/CommentItem.vue
@@ -2,7 +2,7 @@
   <div class="comment">
     <div class="comment__top">
       <div class="comment__user">
-        <div class="comment__avatar bg-cover" v-if="avatar" :style="{backgroundImage:`url(avatar)`}"></div>
+        <div class="comment__avatar bg-cover" v-if="avatar" :style="{backgroundImage:`url(${avatar})`}"></div>
         <div class="comment__name">{{name}}</div>
       </div>
       <div class="comment__time">{{time | time}}</div>
