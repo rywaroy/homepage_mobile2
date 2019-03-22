fd69e3e7f42235492976fecf7bfe97a91339385b
zhangzhihao <464485682@qq.com>
2019-03-22 16:59:55 +0800

    fix: https

--- src/common/axios.js
+++ src/common/axios.js
@@ -5,7 +5,8 @@ import qs from 'qs';
 
 const Axios = axios.create({
   // baseURL: 'http://localhost:3001/api/',
-  baseURL: 'http://www.3zsd.com/api/',
+  // baseURL: 'http://www.3zsd.com/api/',
+  baseURL: 'https://www.3zsd.com/api/',
   timeout: 10000,
   responseType: 'json',
   //   withCredentials: true, // 是否允许带cookie这些
