716454559dee1a8b924bf0f882032857a7e7827c
张志豪 <22761@etransfar.com>
2020-02-11 16:38:55 +0800

    feat: 答案验证

--- src/views/IGuess/IGuess.vue
+++ src/views/IGuess/IGuess.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <my-header title="猜物品"></my-header>
+    <my-header title="猜物品" :back="true"></my-header>
     <div class="m-content scroll" id="scroll">
       <div class="guess__tip">
         <span class="guess__tip-link" @click="showTip = true">游戏说明</span>
       </div>
-      <div class="guess__answar">
-        <div class="guess__answar-item" v-for="item in answar" :key="item.id" @click="cancelAnswar(item)">{{item.content}}</div>
+      <div class="guess__answer">
+        <div class="guess__answer-item" v-for="item in answer" :key="item.id" @click="cancelAnswer(item)">{{item.content}}</div>
       </div>
       <p class="guess__list-tip">点击选择</p>
       <div class="guess__list">
-        <div :class="['guess__list-item', item.select ? 'disable' : '']" v-for="item in list" :key="item.tid" @click="selectAnswar(item)">{{item.title}}</div>
+        <div :class="['guess__list-item', item.select ? 'disable' : '']" v-for="item in list" :key="item.tid" @click="selectAnswer(item)">{{item.title}}</div>
       </div>
       <div class="guess__button-box">
-        <div class="guess__button" @click="verify()">提交回答</div>
+        <div :class="['guess__button', tryList.length >= 6 ? 'disable' : '']" @click="verify()">提交回答</div>
       </div>
       <div class="guess__table">
         <table>
@@ -32,7 +32,7 @@
         </table>
       </div>
       <div class="guess__button-box">
-        <div class="guess__button">提交答案</div>
+        <div class="guess__button" @click="submit()">提交答案</div>
       </div>
     </div>
     <div class="pop__tip-mask" v-if="showTip">
@@ -57,7 +57,7 @@ export default {
   data() {
     return {
       showTip: false,
-      answar: [{ id: 1, content: '', gid: null }, { id: 2, content: '', gid: null }, { id: 3, content: '', gid: null }],
+      answer: [{ id: 1, content: '', gid: null }, { id: 2, content: '', gid: null }, { id: 3, content: '', gid: null }],
       tryList: [],
       right: [],
       list: [
@@ -106,9 +106,9 @@ export default {
   methods: {
     // 初始化
     init() {
-      for (let i = 0; i < this.answar.length; i++) {
-        this.answar[i].content = '';
-        this.answar[i].gid = null;
+      for (let i = 0; i < this.answer.length; i++) {
+        this.answer[i].content = '';
+        this.answer[i].gid = null;
       }
       for (let i = 0; i < this.list.length; i++) {
         this.list[i].select = false;
@@ -118,6 +118,7 @@ export default {
     reset() {
       this.init();
       this.right = this.shuffle();
+      this.tryList = [];
     },
     // 洗牌
     shuffle() {
@@ -136,16 +137,16 @@ export default {
       return rightArr;
     },
     // 选择答案
-    selectAnswar(item) {
+    selectAnswer(item) {
       const { gid, title, select } = item;
       if (select) {
         return;
       }
       let flag = false;
-      for (let i = 0; i < this.answar.length; i++) {
-        if (!this.answar[i].gid) {
-          this.answar[i].gid = gid;
-          this.answar[i].content = title;
+      for (let i = 0; i < this.answer.length; i++) {
+        if (!this.answer[i].gid) {
+          this.answer[i].gid = gid;
+          this.answer[i].content = title;
           flag = true;
           break;
         }
@@ -155,7 +156,7 @@ export default {
       }
     },
     // 取消答案
-    cancelAnswar(item) {
+    cancelAnswer(item) {
       if (!item.gid) {
         return;
       }
@@ -174,19 +175,19 @@ export default {
         this.$toast('尝试次数已经用完');
         return;
       }
-      const res = this.answar.every(item => item.gid);
+      const res = this.answer.every(item => item.gid);
       if (!res) {
         this.$toast('请将回答填写完整');
+        return;
       }
-      // const answar = this.answar.map(item => item.gid);
       let rightNum = 0;
       let positionNum = 0;
-      for (let i = 0; i < this.answar.length; i++) {
-        if (this.answar[i].gid === this.right[i].gid) {
+      for (let i = 0; i < this.answer.length; i++) {
+        if (this.answer[i].gid === this.right[i].gid) {
           positionNum++;
         }
         for (let j = 0; j < this.right.length; j++) {
-          if (this.answar[i].gid === this.right[j].gid) {
+          if (this.answer[i].gid === this.right[j].gid) {
             rightNum++;
             break;
           }
@@ -195,9 +196,35 @@ export default {
       this.tryList.push({
         rightNum,
         positionNum,
-        content: this.answar.map(item => item.content).join(','),
+        content: this.answer.map(item => item.content).join(','),
       });
       this.init();
+    },
+    // 提交回答
+    submit() {
+      const res = this.answer.every(item => item.gid);
+      if (!res) {
+        this.$toast('请将回答填写完整');
+        return;
+      }
+      const right = this.right.map(item => item.gid);
+      const answer = this.answer.map(item => item.gid);
+      let message = '';
+      if (JSON.stringify(right) === JSON.stringify(answer)) {
+        message = '厉害嗷，这你都推理出来了';
+      } else {
+        const text = this.right.map(item => item.title).join(',');
+        message = `太菜了吧，正确答案是 ${text}`;
+      }
+      this.$msg({
+        title: '提示',
+        message,
+        confirmButtonText: '重新开始',
+      }).then(action => {
+        if (action === 'confirm') {
+          this.reset();
+        }
+      });
     },
   },
   components: {
