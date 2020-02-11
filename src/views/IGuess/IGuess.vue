7203c67f0e8b5e72d83d70f3b076020b5e428381
张志豪 <22761@etransfar.com>
2020-02-11 15:03:09 +0800

    Feat: I guess game ui

--- src/views/IGuess/IGuess.vue
+++ src/views/IGuess/IGuess.vue
@@ -2,7 +2,16 @@
   <div>
     <my-header title="猜物品"></my-header>
     <div class="m-content scroll" id="scroll">
-
+      <div class="guess__tip">
+        <span class="guess__tip-link">游戏说明</span>
+      </div>
+      <div class="guess__answar">
+        <div class="guess__answar-item" v-for="item in answar" :key="item.id">{{item.content}}</div>
+      </div>
+      <p class="guess__list-tip">点击选择</p>
+      <div class="guess__list">
+        <div class="guess__list-item" v-for="item in list" :key="item.tid">{{item.title}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +22,39 @@ import Header from '@/components/Header/Header.vue';
 export default {
   data() {
     return {
+      answar: [{ id: 1, content: '', gid: null }, { id: 2, content: '', gid: null }, { id: 3, content: '', gid: null }],
+      list: [
+        {
+          gid: 1,
+          title: '苹果',
+          select: false,
+        },
+        {
+          gid: 2,
+          title: '香蕉',
+          select: false,
+        },
+        {
+          gid: 3,
+          title: '牛奶',
+          select: false,
+        },
+        {
+          gid: 4,
+          title: '雪梨',
+          select: false,
+        },
+        {
+          gid: 5,
+          title: '橘子',
+          select: false,
+        },
+        {
+          gid: 6,
+          title: '蛋糕',
+          select: false,
+        },
+      ],
     };
   },
   mounted() {
@@ -26,4 +68,5 @@ export default {
 </script>
 
 <style>
+@import './iguess.css';
 </style>
