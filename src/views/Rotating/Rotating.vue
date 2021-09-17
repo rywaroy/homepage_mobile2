01d3042e62c3703801a4d0db6ff7a9e7f473b9ff
张志豪 <464485682@qq.com>
2021-09-17 17:46:34 +0800

    feat: 页面布局

--- src/views/Rotating/Rotating.vue
+++ src/views/Rotating/Rotating.vue
@@ -1,7 +1,78 @@
 <template>
   <div>
     <my-header title="旋转拼图" :back="true"></my-header>
-    <div class="m-content">
+    <div class="m-content c-rotating__content">
+      <div class="c-rotating__container">
+        <div class="c-rotating__box">
+          <div v-for="(row, index) in list" :key="index" class="c-rotating__box-line">
+            <div v-for="(col, i) in row" :key="i" :style="{ backgroundColor: col}" class="c-rotating__block"></div>
+          </div>
+        </div>
+        <div class="c-rotating__btns">
+          <div v-for="(row, i) in btns" :key="i" class="c-rotating__btns-line">
+            <div v-for="(col, j) in row" :key="j" class="c-rotating__btn" :style="{ left: `${j + 1.5}rem`, top: `${i + 1.5}rem`}"></div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<script>
+import Header from '@/components/Header/Header.vue';
+import colors from './colors';
+
+export default {
+  components: {
+    MyHeader: Header,
+  },
+  data() {
+    return {
+      type: 4,
+      list: [],
+      colors,
+    };
+  },
+  created() {
+    this.create();
+    this.disrupt();
+  },
+  methods: {
+    create() {
+      this.list = [];
+      for (let i = 0; i < this.type; i++) {
+        this.list[i] = [];
+        for (let j = 0; j < this.type; j++) {
+          this.list[i].push(this.colors[i][j]);
+        }
+      }
+      this.btns = [];
+      for (let i = 0; i < this.type - 1; i++) {
+        this.btns[i] = [];
+        for (let j = 0; j < this.type - 1; j++) {
+          this.btns[i].push('');
+        }
+      }
+    },
+    run(i, j) {
+      const b1 = this.list[i][j];
+      const b2 = this.list[i][j + 1];
+      const b3 = this.list[i + 1][j + 1];
+      const b4 = this.list[i + 1][j];
+      this.list[i][j] = b4;
+      this.list[i][j + 1] = b1;
+      this.list[i + 1][j + 1] = b2;
+      this.list[i + 1][j] = b3;
+    },
+    disrupt() {
+      for (let k = 0; k < this.type * 5; k++) {
+        const i = Math.floor(Math.random() * (this.type - 1));
+        const j = Math.floor(Math.random() * (this.type - 1));
+        this.run(i, j);
+      }
+    },
+  },
+};
+</script>
+<style>
+@import './rotating.css';
+</style>
---
+++ src/views/Rotating/colors.js
@@ -0,0 +1,10 @@
+const colors = [
+  ['red', 'red', 'blue', 'blue', 'green', 'green'],
+  ['red', 'red', 'blue', 'blue', 'green', 'green'],
+  ['orange', 'orange', 'yellow', 'yellow', 'purple', 'purple'],
+  ['orange', 'orange', 'yellow', 'yellow', 'purple', 'purple'],
+  ['turquoise', 'turquoise', 'darkgoldenrod', 'darkgoldenrod', 'darksalmon', 'darksalmon'],
+  ['turquoise', 'turquoise', 'darkgoldenrod', 'darkgoldenrod', 'darksalmon', 'darksalmon'],
+];
+
+export default colors;
